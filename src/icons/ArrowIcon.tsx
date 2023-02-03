2cc5e0d54e89a497e6bd6ec2674f686894e4bc4c
j0sephh123 <iossif123gg@gmail.com>
2023-02-03 09:35:49 +0200

    some styling on calendar component

---
+++ src/icons/ArrowIcon.tsx
@@ -0,0 +1,24 @@
+import classes from "./classes.module.css";
+
+type Props = {
+  orientation: "left" | "right";
+};
+
+export default function ArrowIcon({ orientation }: Props) {
+  return (
+    <svg
+      className={classes[`ArrowIcon__${orientation}`]}
+      width="7"
+      height="10"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6.342.886L2.114 5.114l4.228 4.228"
+        stroke="#9277FF"
+        strokeWidth="2"
+        fill="none"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Calendar from "react-calendar";
 import { useState } from "react";
+import ArrowIcon from "@/icons/ArrowIcon";
+import { months } from "@/utils/date";
 
 export default function Home() {
   const [value, onChange] = useState(new Date());
@@ -7,8 +9,13 @@ export default function Home() {
     <div className="app">
       <div className="calendarContainer">
         <Calendar
+          formatMonthYear={(_, date) =>
+            `${months[date.getMonth()]} ${date.getFullYear()}`
+          }
           showWeekNumbers={false}
+          prevLabel={<ArrowIcon orientation="left" />}
           prev2Label={null}
+          nextLabel={<ArrowIcon orientation="right" />}
           next2Label={null}
           onChange={onChange}
           value={value}
@@ -16,7 +23,7 @@ export default function Home() {
           onClickMonth={() => {
             console.log("month click");
           }}
-          minDetail='month'
+          minDetail="month"
         />
       </div>
     </div>
