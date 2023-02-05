09cf83faf642afc64aaa3705ef27f71bca0ba912
j0sephh123 <iossif123gg@gmail.com>
2023-02-05 10:05:30 +0200

    add boilerplate for radix dialog

--- src/components/pages/InvoicePage/InvoicePage.tsx
+++ src/components/pages/InvoicePage/InvoicePage.tsx
@@ -3,19 +3,23 @@ import Controls from "./Controls/Controls";
 import Details from "./Details/Details";
 import classes from "./InvoicePage.module.css";
 import { Invoice } from "@/types";
+import { useState } from "react";
+import DialogDemo from "@/components/Dialog/Dialog";
 
 type Props = {
   invoice: Invoice;
 };
 
 export default function InvoicePage({ invoice }: Props) {
-  console.log(invoice);
+  const [isEditing, setIsEditing] = useState(true);
 
   return (
     <div className={classes.InvoicePage}>
       <GoBack />
       <Controls />
       <Details invoice={invoice} />
+      <div className={classes.overlay}>class</div>
+      <DialogDemo />
     </div>
   );
 }
