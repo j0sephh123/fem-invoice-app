7525a93b01986772e44cb6487c546630cf5cd8d9
j0sephh123 <iossif123gg@gmail.com>
2023-02-05 09:14:05 +0200

    add some structure to invoice view, but looks bad

--- src/components/pages/InvoicePage/Details/Details.tsx
+++ src/components/pages/InvoicePage/Details/Details.tsx
@@ -1,4 +1,5 @@
 import { Invoice } from "@/types";
+import clsx from "clsx";
 import classes from "./Details.module.css";
 
 type Props = {
@@ -27,30 +28,45 @@ export default function Details({
       <div className={classes.row1}>
         <div className={classes.idAndDescription}>
           <div className={classes.id}>{id}</div>
-          <div className={classes.description}>{description}</div>
+          <div className={clsx(classes.description, classes.label)}>
+            {description}
+          </div>
         </div>
         <div className={classes.senderAddress}>
-          <div>{street}</div>
-          <div>{city}</div>
-          <div>{postCode}</div>
-          <div>{country}</div>
+          <div className={classes.label}>{street}</div>
+          <div className={classes.label}>{city}</div>
+          <div className={classes.label}>{postCode}</div>
+          <div className={classes.label}>{country}</div>
         </div>
       </div>
 
       <div className={classes.row2}>
-        <div className={classes.createdAt}>{createdAt}</div>
-        <div className={classes.clientName}>{clientName}</div>
-        <div className={classes.clientEmail}>{clientEmail}</div>
+        <div className={classes.createdAt}>
+          <div className={classes.label}>Invoice Date</div>
+          <div>{createdAt}</div>
+        </div>
+        <div className={classes.clientName}>
+          <div className={classes.label}>Bill To</div>
+          <div>{clientName}</div>
+        </div>
+        <div className={classes.clientEmail}>
+          <div className={classes.label}>Sent to</div>
+          <div>{clientEmail}</div>
+        </div>
       </div>
 
       <div className={classes.row3}>
-        <div className={classes.paymentDue}>{paymentDue}</div>
-        <div className={classes.clientAddress}>
-          <div>{clientStreet}</div>
-          <div>{clientCity}</div>
-          <div>{clientPostCode}</div>
-          <div>{clientCountry}</div>
+        <div className={classes.paymentDue}>
+          <div className={classes.label}>Payment Due</div>
+          <div>{paymentDue}</div>
         </div>
+        <div className={classes.clientAddress}>
+          <div className={classes.label}>{clientStreet}</div>
+          <div className={classes.label}>{clientCity}</div>
+          <div className={classes.label}>{clientPostCode}</div>
+          <div className={classes.label}>{clientCountry}</div>
+        </div>
+        <div />
       </div>
     </div>
   );
